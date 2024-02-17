705984fe6ff09f152a66054bcc83a31a7a479665
hariprasad <hprasad.sakhare@gmail.com>
2024-02-17 17:13:36 +0530

    new

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =  12321;
+let accountEmail = "hariprasad@gmail.com";
+var accountPassword = "123456";
+accountCity = "Nanded";
+
+
+
+accountEmail = "hc@hc.com";
+accountPassword = "121212112";
+accountCity = "Pune";
+
+
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
