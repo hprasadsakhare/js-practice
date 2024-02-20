d0416d42af41d1cbcf3af88807d0329c2f615044
hariprasad <hprasad.sakhare@gmail.com>
2024-02-21 00:03:14 +0530

    Add existing code

---
+++ 01_basics/06nums_math.js
@@ -0,0 +1,21 @@
+/*
+
+const balance = new Number(100)
+console.log(balance);
+
+const score = 400
+console.log(score);
+
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+const otherNumber = 23.87976
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString());
+
+*/
+
+console.log(Math.round(5.6))
+console.log(Math.random());
