74854ab1c2ea590a60974b9657636aa6498700b4
hariprasad <hprasad.sakhare@gmail.com>
2024-02-26 00:14:09 +0530

    First commit

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -15,5 +15,15 @@ function addTwoNumbers(num1,num2){
 }
 
 const result = addTwoNumbers(4,5)
-console.log("Result : " , result);
+//console.log("Result : " , result);
 //addTwoNumbers(5,4)  
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("please enter a useename");
+        returen
+    }
+   return `${username} just logged in`
+}
+
+console.log(loginUserMessage("hariprasad"))
