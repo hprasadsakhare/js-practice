d984e15aa64220b54055112c3ef9fe6e2881638d
hariprasad <hprasad.sakhare@gmail.com>
2024-02-26 09:51:55 +0530

    First commit

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,4 +26,23 @@ function loginUserMessage(username){
    return `${username} just logged in`
 }
 
-console.log(loginUserMessage("hariprasad"))
+//console.log(loginUserMessage("hariprasad"))
+
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+//console.log(calculateCartPrice (200,400,600))
+
+const user ={
+    username: "hariprasad",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price} ` );
+}
+
+handleObject(user)
+
