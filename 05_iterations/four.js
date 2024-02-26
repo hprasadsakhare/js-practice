b7d408a845db1b7d019ba24c85703fee68f0abf2
hariprasad <hprasad.sakhare@gmail.com>
2024-02-27 00:12:15 +0530

    practice

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'javascrfipt',
+    cpp:'c++',
+    py:'python',
+    rbb:'ruby',
+    switch:'swift by apple'
+}
+
+for (const key in myObject){
+//    console.log(`key is ${key} and value is ${myObject[key]}`);
+}
+
+
+const progrtamming = ["js","rb","py","cpp","java"]
+for (const key in progrtamming){
+//    console.log(progrtamming[key]);
+}
+
+/*
+const map = new Map()
+map.set('IN',"india")
+map.set('USA',"United State of America")
+map.set('Fr',"France")
+
+for (const key in map){
+    console.log(key);
+}
+*/
