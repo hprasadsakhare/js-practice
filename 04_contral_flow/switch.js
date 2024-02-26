ac8d06eccba98a4537848e6a68175f61cd90e825
hariprasad <hprasad.sakhare@gmail.com>
2024-02-26 18:39:12 +0530

    practice

---
+++ 04_contral_flow/switch.js
@@ -0,0 +1,27 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+
+        case 2:
+            console.log("feb");
+            break;
+    
+            case 3:
+        console.log("march");
+        break;
+    
+        case 4:
+        console.log("april");
+        break;
+    
+        case 5:
+        console.log("may");
+        break;
+    
+    default:
+        console.log("default case match");
+        break;
+}
---
+++ 04_contral_flow/truthy.js
@@ -0,0 +1,13 @@
+const userEmail = "h@itesh.ai";
+
+if(userEmail) {
+    console.log("got user email");
+} else {
+    console.log("no user email");
+}
+
+
+//if(userEmail.length === 0){
+//    console.log("array is em");
+//}
+
